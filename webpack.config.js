44dca212539e1f8a24e274f61ae8dd4c199eb469
Justin Ellison <eljust@amazon.com>
2021-03-04 12:28:39 -0500

    Adjusted formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,53 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-const outputDir = path.join(__dirname, "dist");
+const outputDir = path.join(__dirname, 'dist');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/react.tsx",
-  target: "web",
-  devtool: "source-map",
+  mode: 'development',
+  entry: './src/react.tsx',
+  target: 'web',
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
         include: /src/,
-        use: ["ts-loader"],
+        use: ['ts-loader']
       },
       {
         test: /\.scss$/,
         include: /src/,
         use: [
+          // injects CSS modules into DOM
           {
             loader: 'style-loader',
-            options: { esModule: false, modules: { namedExport: true } },
+            options: { esModule: false, modules: { namedExport: true } }
           },
+          // loads CSS and handles imports
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: { modules: true }
           },
+          // converts SASS into CSS
           'sass-loader'
-        ],
-      },
-    ],
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     path: outputDir,
-    filename: "react.js",
+    filename: 'react.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
+      template: './src/index.html'
+    })
   ],
   devServer: {
-    contentBase: outputDir,
-  },
+    contentBase: outputDir
+  }
 };
